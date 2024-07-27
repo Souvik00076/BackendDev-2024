e9c8546807503fc4adc41530ee412e926f93f872
Souvik00076 <souvikbhattacharjee00076@gmail.com>
2024-07-27 12:49:03 +0530

    completed Schema creation

--- finance-tracker-backend/app/http/models/Transaction.ts
+++ finance-tracker-backend/app/http/models/Transaction.ts
@@ -1,23 +1,29 @@
 import mongoose, { Schema, Types } from "mongoose";
 import { ITransactions, TRANSACTION_STATUS } from "../../@types";
 
-const transactionSchema = new Schema<ITransactions>({
-  userId: {
-    type: Types.ObjectId
+const transactionSchema = new Schema<ITransactions>(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+    },
+    expenseId: {
+      type: Schema.Types.ObjectId,
+    },
+    transactionType: {
+      type: String,
+      enum: TRANSACTION_STATUS,
+      required: [true, "transaction type cannot be empty"],
+    },
+    amount: {
+      type: Number,
+      required: [true, "Amount cannot be empty"],
+    },
   },
-  expenseId: {
-    type: Types.ObjectId
-  }
-  transactionType: {
-    type: String,
-    enum: TRANSACTION_STATUS,
-    required: [true, "transaction type cannot be empty"]
-  },
-  amount: {
-    type: Number,
-    required: [true, "Amount cannot be empty"]
-  }
-}, { timestamps: true })
+  { timestamps: true },
+);
 
-const Transaction = mongoose.model<ITransactions>('transaction', transactionSchema)
-export default Transaction
+const Transaction = mongoose.model<ITransactions>(
+  "transaction",
+  transactionSchema,
+);
+export default Transaction;
--- finance-tracker-backend/app/http/models/UserExpense.ts
+++ finance-tracker-backend/app/http/models/UserExpense.ts
@@ -1,44 +1,44 @@
-import { Schema } from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import { IUserExpense } from "../../@types";
-import { Types } from 'mongoose'
+import { Types } from "mongoose";
 
-const userExpenseSchema = new Schema<IUserExpense>({
-  userId: {
-    type: Types.ObjectId,
-    ref: 'user',
-    required: [true, "User ID cannot be empty"]
-  },
-  month: {
-    type: String,
-    enum: {
-      'JANUARY',
-      'FEBRUARY',
-      'MARCH',
-      'APRIL',
-      'MAY',
-      'JUNE',
-      'JULY',
-      'AUGUST',
-      'SEPTEMBER',
-      'OCTOBER',
-      'NOVEMBER',
-      'DECEMBER',
+const userExpenseSchema = new Schema<IUserExpense>(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: [true, "User ID cannot be empty"],
     },
-    required: [true, "Month cannot be empty"]
+    month: {
+      type: String,
+      enum: [
+        "JANUARY",
+        "FEBRUARY",
+        "MARCH",
+        "APRIL",
+        "MAY",
+        "JUNE",
+        "JULY",
+        "AUGUST",
+        "SEPTEMBER",
+        "OCTOBER",
+        "NOVEMBER",
+        "DECEMBER",
+      ],
+      required: [true, "Month cannot be empty"],
+    },
+    totalIncome: {
+      type: Number,
+      default: 0,
+    },
+    totalExpense: {
+      type: Number,
+      default: 0,
+    },
   },
-  totalIncome: {
-    type: Number,
-    default: 0
-  },
-  totalExpense: {
-    type: Number,
-    default: 0
-  }
-}, { timestamps: true })
+  { timestamps: true },
+);
 
-const Expense = mongoose.model<IUserExpense>('userexpense', userExpenseSchema)
+const Expense = mongoose.model<IUserExpense>("userexpense", userExpenseSchema);
 
-export default Expense
-
-
-
+export default Expense;
